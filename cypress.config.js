385f23b598e03f956f31082d42586615e6cb8951
Mark Judy <markjudy@ibm.com>
2022-11-09 11:28:23 -0500

    chore: Update packages

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,11 @@
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+
 module.exports = {
   e2e: {
     chromeWebSecurity: false,
     experimentalSessionAndOrigin: true,
-    setupNodeEvents(on, config) {},
+    setupNodeEvents(on, config) {
+      on('file:preprocessor', createBundler({}));
+    },
   },
 }
